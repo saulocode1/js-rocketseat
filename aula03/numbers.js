c7b7211fb9b692bb784c8a4d59bfcee022ba1640
Saulo <sauloveigr@hotmail.com>
2021-06-14 18:32:10 -0300

    Create aula03

---
+++ aula03/numbers.js
@@ -0,0 +1,10 @@
+/*
+    33 // inteiros
+    12.5 // reais - float
+    NaN // Not a number
+    Infinity // infinito
+*/
+console.log(33)
+console.log(12.5)
+console.log(12 / 'asd')
+console.log(Infinity)
