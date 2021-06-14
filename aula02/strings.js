7a99d7a56d019617920b45347b0ab6f39704b374
Saulo <sauloveigr@hotmail.com>
2021-06-14 18:20:17 -0300

    Create aula02

---
+++ aula02/strings.js
@@ -0,0 +1,14 @@
+/* 
+    Cadeia de caracteres
+
+    "" // aspas duplas
+    '' // aspas simples
+    `` // template literals ou template strings
+
+*/
+
+let nome = 'saulo'
+
+console.log(" 'saulo' ")
+console.log(' "saulo" ')
+console.log(`nome = ${nome}`) // pode ser usado aspas simples e duplas; quebra de linhas; expressões ${1+1} (interpolação)
