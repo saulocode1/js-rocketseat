5f9dde603c25d31c536a78dfd7ff19bd4897b1bd
Saulo <sauloveigr@hotmail.com>
2021-06-16 20:08:30 -0300

    Create practicing lesson 04

---
+++ 03 - Practicing/lesson04/objects.js
@@ -0,0 +1,12 @@
+// Object
+
+const person = {
+    name: 'John',
+    age: 30,
+    weight: 80,
+    isAdmin: true
+}
+
+console.log(person.name) // Show name
+
+console.log(`${person.name} has ${person.age} years old.`)
