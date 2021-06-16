3cd0a82c98a9947f20e51e75c769ddea5ccb058c
Saulo <sauloveigr@hotmail.com>
2021-06-16 18:48:23 -0300

    Create practicing lesson 03

--- 03 - Practicing/lesson02/script.js
+++ 03 - Practicing/lesson02/script.js
@@ -1,7 +1,7 @@
-// declaration  
+// agrupamento de declarações 
 let age, isHuman
 
 age = 23
 isHuman = true
 
-console.log(age, isHuman)
+console.log(age, isHuman) // 2 argumentos
---
+++ 03 - Practicing/lesson03/script.js
@@ -0,0 +1,17 @@
+// declaration  
+let name, age, isHuman
+
+name = 'Saulo'
+age = 23
+isHuman = true
+
+// múltiplos argumentos na função
+// console.log(age, isHuman) // 2 argumentos
+
+// escrita do texto + variáveis 
+
+// concatenação
+console.log(name + ' tem ' + age + ' anos.')
+
+// interpolação; template strings
+console.log(`${name} tem ${age} anos.`)
