4e5339480cf5e137d528d0103627656096020bbc
Saulo <sauloveigr@hotmail.com>
2021-06-15 21:39:17 -0300

    Create aula06

---
+++ aula06/object.js
@@ -0,0 +1,17 @@
+/*
+    * Object
+        * Objeto
+        * Propriedades / Atributos
+        * Funcionalidades / Métodos
+        
+
+    { propriedade: "valor" }
+*/  
+
+console.log({
+    nome: 'saulo',
+    idade: 23,
+    andar: () => {
+        console.log('andar')
+    } 
+}) 
