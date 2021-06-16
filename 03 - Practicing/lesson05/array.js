e33330c8eda93392382cf0e0461e52d6770fb104
Saulo <sauloveigr@hotmail.com>
2021-06-16 20:47:52 -0300

    Create practicing lesson 05

---
+++ 03 - Practicing/lesson05/array.js
@@ -0,0 +1,17 @@
+// Array
+
+const animals = [
+    'dog', // 0
+    'cat', // 1
+    'lion', // 2
+    {
+        name: 'monkey',
+        age: 3
+    }
+]
+
+console.log(animals[0]) // dog; position 0
+
+console.log(animals.length) // 2; length
+
+console.log(animals[3].name) // monkey
